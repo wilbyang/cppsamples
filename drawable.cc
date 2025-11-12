3f438d9503b4f1a9f1792cbd100969d9e9c69f94
wilbyang <yang.wilby@gmail.com>
2025-11-12 09:50:07 +0100

    empty base optimization

--- drawable.cc
+++ drawable.cc
@@ -3,12 +3,12 @@
 #include <utility>
 #include <vector>
 
-// 1) 抽象接口（只暴露客户需要的语法）
+
 class Drawable {
 public:
     void draw() const { ptr_->draw(); }
     template<class T>
-    Drawable(T&& obj)               // 2) 任意类型进入
+    Drawable(T&& obj)
         : ptr_(std::make_shared<Model<T>>(std::forward<T>(obj))) {}
 private:
     struct Concept {
@@ -24,13 +24,13 @@ private:
     std::shared_ptr<const Concept> ptr_;
 };
 
-// 3) 新类型 = 只写自己的 draw，不改 Drawable
+
 struct Circle { void draw() const { std::puts("○"); } };
 struct Square { void draw() const { std::puts("□"); } };
 
 int main() {
     std::vector<Drawable> v{ Circle{}, Square{} };
-    for (auto& d : v) d.draw();   // 多态，值语义
+    for (auto& d : v) d.draw();
 }
 // 场景：运行期多态（动态分发）
 // 目标：值语义、无裸指针、可序列化、可反射
